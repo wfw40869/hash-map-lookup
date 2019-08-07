ba6e3afc7588a638e4ae24d72ee994b7ac114edb
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-08-07 15:19:50 -0500

    Added comments to remember what I wrote and refactored to make code more readable

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,11 @@ import carSales from "./data.js"
 const totalProfits = carSales.map(sale => sale.gross_profit).reduce((total, indvSale) => total += indvSale, 0)
 console.log('totalProfits: ', totalProfits)
 
-const monthsPurchasedArr = carSales.map(car => car.purchase_date.slice(5, 7))
+
+
+
+
+// CREATE MAP DATA STRUCTURE FUNCTIONS
 
 const makeNewMap = (array, incrementer, startingValue) => {
     const map = new Map()
@@ -18,12 +22,6 @@ const makeNewMap = (array, incrementer, startingValue) => {
 }
 
 
-const monthsPurchasedMap = makeNewMap(monthsPurchasedArr, 1, 1)
-
-
-// console.log(monthsPurchasedMap)
-
-
 const findMax = (map) => {
     let maxKey, count = 0
     map.forEach((value, key) => {
@@ -35,29 +33,41 @@ const findMax = (map) => {
     return maxKey
 }
 
+const makeProfitMap = (carSales) => {
+    const map = new Map()
+    carSales.forEach(car => {
+        if (map.has(car.sales_agent.last_name)) {
+            map.set(car.sales_agent.last_name, (map.get(car.sales_agent.last_name)) + car.gross_profit)
+        } else {
+            map.set(car.sales_agent.last_name, car.gross_profit)
+        }
+    })
+    return map  
+}
+// FINDS THE MONTHS WITH THE MOST CARS SOLD
+const monthsPurchasedArr = carSales.map(car => car.purchase_date.slice(5, 7))
+const monthsPurchasedMap = makeNewMap(monthsPurchasedArr, 1, 1)
 console.log("Month with most cars sold is: ", findMax(monthsPurchasedMap))
 
-
+// FINDS THE REP WITH THE MOST CARS SOLD
 const lastNames = carSales.map(car => car.sales_agent.last_name)
-
 const lastNamesMap = makeNewMap(lastNames, 1, 1)
-
 console.log("Rep with most cars sales is: ", findMax(lastNamesMap))
 
-const individualProfitsMap = makeNewMap(carSales.map(car => car.sales_agent.last_name), parseFloat(carSales.map(car => car.gross_profit)), 0)
+// const test = parseFloat(carSales.map(car => car.gross_profit))
+// console.log('TEST: ', test)
 
+
+const individualProfitsMap = makeProfitMap(carSales)
 console.log('individualProfitsMap: ', individualProfitsMap)
-
 console.log("The Rep with the most gross profits is: ", findMax(individualProfitsMap))
 
 const carModelsMap = makeNewMap(carSales.map(car => car.vehicle.model), 1, 1)
-
 console.log('carModelsMap: ', carModelsMap)
 console.log(`The most popluar car was: ${findMax(carModelsMap)}`)
 
 const creditProvidersMap = makeNewMap(carSales.map(car => car.credit.credit_provider), 1, 1)
 console.log('creditProvidersMap: ', creditProvidersMap)
-
 console.log(`The most popluar credit provider was: ${findMax(creditProvidersMap)}`)
 
 
